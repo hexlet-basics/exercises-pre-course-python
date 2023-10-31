115b611f608d58925213994be65ca497b251e0a7
Ivan Gagarinov <dzencot@gmail.com>
2023-10-31 14:48:42 +0500

    Update test_code.py

--- modules/50-loops/30-syntactic-sugar/test_code.py
+++ modules/50-loops/30-syntactic-sugar/test_code.py
@@ -6,3 +6,4 @@ def test1():
     assert index.filter_string(text, 'w') == 'If I look back I am lost'
     assert index.filter_string(text, 'I') == 'f  look back  am lost'
     assert index.filter_string('zz zorro', 'z') == ' orro'
+    assert index.filter_string(text, 'i') == text
